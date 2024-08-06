64c30b40d3c468ef42ba8d0891eee0817f0ac997
Souriana-Shakkour <99126874+Souriana-Shakkour@users.noreply.github.com>
2024-08-06 17:15:35 +0200

    create-graph.py

--- Data/create-graph.py
+++ Data/create-graph.py
@@ -1,100 +1,65 @@
-!pip install numpy
-!pip install time
-!pip install tqdm
+import numpy 
+import random
+import winsound
 
-from google.colab import drive
-drive.mount('/content/drive/')
-
-import numpy as np
-from tqdm.notebook import tqdm
-import time
-
-class Node:
-	def __init__(self, data):
-		self.data = data
-		self.next = None
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        
-    def insertAtEnd(self, data):
-        newNode = Node(data)
-        if self.head is None:
-            self.head = newNode
-            return
-        currentNode = self.head
-        while(currentNode.next):
-            currentNode = currentNode.next
-        currentNode.next = newNode
-
-class Graph():
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [LinkedList() for column in range(vertices)]
-
-    def minDistance(self, dist, sptSet):
-        print(self.V)
-        min = float('inf')
-        print(self.V)
-        for v in range(self.V):
-            if dist[v] < min and sptSet[v] == False:
-                min = dist[v]
-                minIndex = v
-        return minIndex
-
-    def dijkstra(self, src):
-        dist = [float('inf')] * self.V
-        if len(dist) != 0:
-            dist[src] = 0
-        sptSet = [False] * self.V
-        for count in range(self.V):
-            u = self.minDistance(dist, sptSet)
-            sptSet[u] = True
-            v = self.graph[u].head
-            while v != None:
-                dis = v.data[1]
-                neighbour = v.data[0]
-                if (sptSet[neighbour] == False and       # not visited
-				dist[neighbour] > dist[u] + int(dis)):   #shorter path found
-                    dist[neighbour] = dist[u] + int(dis)
-                v = v.next
-
-def csvToLinkedList (fileName):
-    arr = np.loadtxt("/content/drive/Shareddrives/dijkstra_bp/final/"+fileName, delimiter=",", dtype=str)
-    arrint = [LinkedList() for column in range(len(arr))]
+def arrayToCSV (array, csv):
+    numpy.savetxt(csv, array, delimiter=',', newline= '\n')
+    
+def generateLinearGraphCSV (csv, size):
+    e = [[0 for i in range(size)]
+         for j in range (size)]
+    for i in range(size):
+         for j in range (size):
+             if i == j-1:
+                 e[i][j] = random.randint(1, 10)
+    arrayToCSV(e, csv)
+      
+def csvToMatrix (fileName):
+    arr = numpy.loadtxt(fileName, delimiter=",", dtype=str)
+    arrint = [[None for column in range(len(arr))]
+                for row in range(len(arr))]
     rowIndex = 0
-    for rowIn in tqdm(range(0,len(arr)), leave = True, desc="creating graph", ascii=True, ncols=75, dynamic_ncols=True):
+    for row in arr:
         columnIndex = 0
-        row = arr[rowIn]
         for column in row:
-            if (float(arr[rowIndex][columnIndex]) != 0):
-                arrint[rowIndex].insertAtEnd((columnIndex, float(arr[rowIndex][columnIndex])))
+            arrint[rowIndex][columnIndex] = float(arr[rowIndex][columnIndex])
             columnIndex += 1
         rowIndex += 1
-        columnIndex = 0
     return arrint
+    
+def densifyCSV (oldCSV, newCSV ,numVertices, numNewEdges):
+    if numNewEdges == 0:
+        return
+    e = csvToMatrix(oldCSV)
+    numAddedEdges = 0
+    for row in range (len(e)):
+        for column in range (len(e[row])):
+            if (e[row][column] == 0 ):
+                e[row][column] = random.randint(1, 10)
+                numAddedEdges += 1
+                if numAddedEdges == numNewEdges:
+                    arrayToCSV(e, newCSV)
+                    return
+    arrayToCSV(e, newCSV)
 
-def saveRunningTime (csv):
-    g = Graph(0)
-    g.graph = csvToLinkedList(csv)
-    g.V = len(g.graph)
-    t0 = time.perf_counter()
-    g.dijkstra(0)
-    t1 = time.perf_counter()
-    total = t1-t0
-    file = open("/content/drive/Shareddrives/dijkstra_bp/final/time.txt","a")
-    file.write(str(csv) + " time for linked list is: " + str(total) + '\n')
-    file.close()
+def createGraph (size, numGraphs, alaramOn):
+    fileName = str(size) + ".1.csv"
+    numEdgesToAdd = ((size * (size - 1) ) - (size - 1) )
+    numNewEdgesPerGraph = numEdgesToAdd // (numGraphs - 1)
+    generateLinearGraphCSV(fileName, size)
+    for graphCount in range(1, numGraphs + 1):
+        newFileName = str(size) + "." + str(graphCount+1) + ".csv"
+        densifyCSV(fileName, newFileName, size, numNewEdgesPerGraph)
+        fileName = newFileName
+    if (numEdgesToAdd % numGraphs) != 0:
+        densifyCSV(fileName, newFileName, size, numEdgesToAdd % numGraphs)
+    if alaramOn == "Y" or alaramOn == "y":
+        frequency = 2500
+        duration = 1200
+        winsound.Beep(frequency, duration)
 
-def runForGraphs (numGraphs, numVertices):
-    for i in tqdm (range (1, numGraphs+ 1),
-                  desc="Loading…",
-                  ascii=False):
-      saveRunningTime(str(numVertices) + "." + str(i) + ".csv")
-      
-runForGraphs (16, 2500)
-runForGraphs (21, 5000)
-runForGraphs (26, 10000)
+createGraph(2500, 15, "y")
+createGraph(5000, 20, "y")
+createGraph(10000, 25, "y")
 
 print ("Done")
